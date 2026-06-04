83e183d353b890f5a7137487920679c5c038812e
SillyTavern Sync <sillytavern-sync@example.com>
2026-06-04 12:37:47 +0800

    debug: print SQL_DSN on startup

--- model/main.go
+++ model/main.go
@@ -121,6 +121,7 @@ func chooseDB(envName string, isLog bool) (*gorm.DB, error) {
 	}()
 	dsn := os.Getenv(envName)
 	if dsn != "" {
+		common.SysLog(fmt.Sprintf("DB DSN (%s): %s", envName, dsn))
 		if strings.HasPrefix(dsn, "postgres://") || strings.HasPrefix(dsn, "postgresql://") {
 			// Use PostgreSQL
 			common.SysLog("using PostgreSQL as database")
